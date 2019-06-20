1594e8ea2236fa2bf1d18dd6bdc72e08143d2293
Simo Savonen <sipesa@utu.fi>
2019-06-20 08:12:29 +0300

    style tweaks

--- src/components/CowList.js
+++ src/components/CowList.js
@@ -6,10 +6,21 @@ const CowList = inject("cowsStore")(
   observer(({ cowsStore }) => {
     return (
       <div className="section">
-        {cowsStore.cows.map(cow => (
-          <CowRow key={cow.id} cow={cow} />
-        ))}
-      </div>
+        <div className="columns is-centered">
+          <div className="column is-8">
+
+            <div className="columns is-centered" >
+              <div className="column is-2" style={{ paddingBottom: "3px"}}></div>
+              <div className="column is-6" style={{ paddingBottom: "3px"}}>Nimi</div>
+            </div>
+
+            {cowsStore.cows.map(cow => (
+              <CowRow key={cow.id} cow={cow} />
+            ))}
+
+          </div>
+        </div>
+      </div>      
     );
   })
 );
--- src/components/CowRow.js
+++ src/components/CowRow.js
@@ -3,17 +3,15 @@ import { observer } from "mobx-react";
 import { ReactComponent as CowHead } from "./cow.svg";
 
 const rowStyles = {
-  border: "1px solid blue",
-  borderRadius: "10px",
-  padding: "5px",
-  margin: "5px"
+  border: "1px solid #6497b1",
+  borderRadius: "10px"
 };
 
 const CowRow = observer(({ cow }) => {
   return (
-    <div className="columns is-mobile is-centered" style={{ ...rowStyles }}>
+    <div className="columns is-mobile is-centered is-vcentered" style={{ ...rowStyles }}>
       <div className="column is-2">
-        <CowHead height={50} />
+        <CowHead height={40} />
       </div>
       <div className="column is-6">{cow.name}</div>
     </div>
